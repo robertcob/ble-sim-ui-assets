9e9378760721318c5f9cb2fa9bdfe9bd993f7353
robertcob <robertcob@icloud.com>
2022-04-01 22:49:44 +0100

    final gui loadout

--- sim-gui.py
+++ sim-gui.py
@@ -1,19 +1,35 @@
 from tkinter import *
-from textwrap import fill
-# from tkinter.ttk import *
 from tkinter.font import *
 from tk_extensions import ScrollableTV
 
-
 root = Tk()
+root.resizable(0, 0)
 root.title("ble-simulation")
 ff10=Font(family="Consolas", size=10)
 ff10b=Font(family="Consolas", size=10, weight=BOLD)
 
-menu = Menu(root, border=2)
+
+#### Attack Model Selection Menu...
+def initNoAttackSim():
+    print("creating sim of ble 5, no attacker")
+
+def initFalseSensorAttackSim():
+    print("creating false sensor attack simulation")
+
+
+menu = Menu(root)
 root.config(menu=menu)
 subMenu = Menu(menu)
 menu.add_cascade(label="Attack Models", menu=subMenu)
+
+sec0SubMenu = Menu(subMenu)
+sec1SubMenu = Menu(subMenu)
+
+subMenu.add_cascade(label="Security Level 0", menu=sec0SubMenu)
+subMenu.add_cascade(label="Security Level 1", menu=sec1SubMenu)
+
+sec0SubMenu.add_command(label="no attacks", command=initNoAttackSim)
+sec0SubMenu.add_command(label="false sensor", command=initFalseSensorAttackSim)
 
 masterFrame =Frame(root)
 masterFrame.grid(row=0)
@@ -25,6 +41,8 @@ nrows = 10
 cellW = winW / ncols
 cellH = winH / nrows
 
+
+### Grid for node layout
 class Node:
     def __init__(self, row, col):
         self.row = row
@@ -55,7 +73,7 @@ canvas = Canvas(masterFrame, width=winW, height=winH,
                    borderwidth=0, highlightthickness=0, bg="white")
 canvas.grid( row=0, column=0, sticky=EW)
 
-# init a scrollabletv
+# init a scrollabletv for packet transfers...
 bottomMasterFrame = Frame(root)
 bottomMasterFrame.grid(row=1)
 tv1=ScrollableTV(bottomMasterFrame, selectmode=BROWSE, height=4, show="tree headings", columns=("Time", "Sensor", "Task", "Payload"), style="Foo2.Treeview")
@@ -84,7 +102,7 @@ sb1.configure(command=tv1.xview)
 sideContentFrame = Frame(masterFrame)
 sideContentFrame.grid( row = 0, column=1)
 
-## init a scrollabletv for output table
+## init a scrollabletv for output table of sensor events
 tv2=ScrollableTV(sideContentFrame, selectmode=BROWSE, height=13, show="tree headings", columns=("Time", "Sensor", "Message"), style="Foo2.Treeview")
 tv2.heading("Time", text="Time", anchor=W)
 tv2.heading("Sensor", text="Sensor", anchor=W)
@@ -93,36 +111,35 @@ tv2.column("#0", width=0, stretch=False)
 tv2.column("Time", width=80, stretch=False)
 tv2.column("Sensor", width=80, stretch=False)
 tv2.column("Message", minwidth=1400, width=220, stretch=True)
-# tv2.grid(row=0, column=0, padx=8, pady=(8,0))
 tv2.grid(padx=8, pady=(8,0))
 
-# style config. use a ScrollableStyle and pass in the ScrollableTV whose configure needs to be managed. if you had more than one ScrollableTV, you could modify ScrollableStyle to store a list of them and operate configure on each of them
 s2=ScrollableTV.ScrollableStyle(tv2)
 s2.configure("Foo2.Treeview", font=ff10, padding=1)
 s2.configure("Foo2.Treeview.Heading", font=ff10b, padding=1)
 
 # init a scrollbar
 sb2=Scrollbar(sideContentFrame, orient=HORIZONTAL)
-##sb2.grid(row=1, sticky=EW, padx=8, pady=(0,8))
 sb2.grid(row=1, sticky=EW, padx=8, pady=(0,8))
 tv2.configure(xscrollcommand=sb2.set)
 sb2.configure(command=tv2.xview)
 
-notes = Text(root, width=50, height=10, bg="white")
-# notes.grid(row=1, column=1)
 
+### creation of grid
 grid = generatGrid(nrows, ncols)
 drawGrid(canvas, grid)
 
+
+### control menu button onclick methods
 def start():
     pass
 
 def stop():
     pass
 
-def reload():
+def reset():
     pass
 
+### simulation control frame
 rootFrame= Frame(sideContentFrame, highlightbackground="#F6F5F5", highlightthickness=2)
 rootFrame.grid(row=2)
 topFrame = Frame(rootFrame, bg="grey", width=380, height=60)
@@ -145,9 +162,9 @@ stopbutton = Button(topInnerFrame2, text="Stop", fg="black", bg="grey")
 
 stopbutton.grid(padx=22, pady=5)
 
-reloadButton = Button(topInnerFrame3, text="Reload", fg="black", bg="grey")
+resetButton = Button(topInnerFrame3, text="Reset", fg="black", bg="grey")
 
-reloadButton.grid(padx=23, pady=5)
+resetButton.grid(padx=23, pady=5)
 
 timeVar = StringVar()
 timeVar.set("0.00")
@@ -155,7 +172,5 @@ timeLabel = Label(bottomframe, fg="black",  text= "Time:")
 vartimeLabel = Label(bottomframe, fg="black", textvariable=timeVar)
 timeLabel.grid(row= 0,column=0)
 vartimeLabel.grid(row=0, column=1)
-
-
 
 root.mainloop()
